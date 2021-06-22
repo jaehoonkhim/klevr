db09d065f785b0b95bc8c968da00cb8a673f8ada
JaehoonKim <jaehoon@nexclipper.io>
2021-06-22 11:28:42 +0900

    iteration task에 대한 test(example) 추가

--- pkg/common/task_executor_test.go
+++ pkg/common/task_executor_test.go
@@ -6,7 +6,7 @@ import (
 	concurrent "github.com/orcaman/concurrent-map"
 )
 
-func Test_taskExecutor_execute(t *testing.T) {
+func Test_taskExecutor_execute_normalTask(t *testing.T) {
 	type fields struct {
 		//RWMutex      sync.RWMutex
 		runningTasks concurrent.ConcurrentMap
@@ -33,6 +33,63 @@ func Test_taskExecutor_execute(t *testing.T) {
 						Name:           "Hello-Command",
 						TaskType:       TaskType(AtOnce),
 						AgentKey:       "agent-1234",
+						TotalStepCount: 1,
+						Parameter:      "",
+						Steps: []*KlevrTaskStep{
+							&KlevrTaskStep{
+								ID:          0,
+								Seq:         1,
+								CommandName: "Hello",
+								CommandType: CommandType(INLINE),
+								Command:     `echo "hello" >> ~/hello.txt`,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			executor := &taskExecutor{
+				//RWMutex:      tt.fields.RWMutex,
+				runningTasks: tt.fields.runningTasks,
+				updatedTasks: tt.fields.updatedTasks,
+				closed:       tt.fields.closed,
+			}
+			executor.execute(tt.args.tw)
+		})
+	}
+}
+
+func Test_taskExecutor_execute_iterationTask(t *testing.T) {
+	type fields struct {
+		//RWMutex      sync.RWMutex
+		runningTasks concurrent.ConcurrentMap
+		updatedTasks Queue
+		closed       bool
+	}
+	type args struct {
+		tw *TaskWrapper
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		// TODO: Add test cases.
+		{
+			"IterationTask",
+			fields{runningTasks: concurrent.New(), updatedTasks: *NewMutexQueue()},
+			args{
+				tw: &TaskWrapper{
+					KlevrTask: &KlevrTask{
+						ID:             2,
+						ZoneID:         3,
+						Name:           "Hello-Command",
+						TaskType:       TaskType(Iteration),
+						AgentKey:       "agent-1234",
+						Cron:           "* * * * *",
 						TotalStepCount: 1,
 						Parameter:      "",
 						Steps: []*KlevrTaskStep{
